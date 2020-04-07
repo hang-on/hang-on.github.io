f9580e3b0e3a7851558d960c0939caa8ce32872a
hang-on <hang-on@users.noreply.github.com>
2020-04-07 20:38:26 +0200

    q

--- whq/globals.js
+++ whq/globals.js
@@ -3,5 +3,6 @@
 var heroes = new Array("Mistweaver Saih","Tenebrael Shard",
                       "Fyreslayer Doomseeker","Knight Questor");
 
+var focusHero = 0;
 
 // document.getElementById("result").innerHTML = localStorage.getItem("Party");
