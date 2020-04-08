5e600f6488ace2174c274be8595c075ed1a78ce5
hang-on <hang-on@users.noreply.github.com>
2020-04-08 16:14:08 +0200

    d

--- whq/globals.js
+++ whq/globals.js
@@ -6,6 +6,7 @@ var heroes = new Array("Mistweaver Saih","Tenebrael Shard",
 var focusHero = ""; // Name of focus Hero
 var focusHeroSlot = 0; // Active slot 0-3.
 var heroesTotal = 4 // Heroes
-var skillsTotal = 6 // Skills 
+var skillsTotal = 6 // Skills
+var itemsTotal = 6 // Items 
 
 // document.getElementById("result").innerHTML = localStorage.getItem("Party");
