a302cfc7d0c3c4559314e2d84178b829bc7960b1
hang-on <hang-on@users.noreply.github.com>
2020-04-10 16:44:12 +0200

    sd

--- views/javascript.js
+++ views/javascript.js
@@ -1,3 +1,5 @@
+
+
 function alertWindowDimension(){
   var w = getWindowWidth();
   var h = getWindowWHeight();
@@ -15,5 +17,20 @@ function getWindowWHeight(){
   || document.body.clientHeight;
   return h;
 }
-function initialize(){
+function initializeHeroPage(){
+  // Populate the Hero Menu
+  var heroNames = [];
+  heroNames = getHeroNames();
+  list = document.getElementById("heroMenu");
+  var i;
+  for (i = 0; i < heroNames.length; i++){
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(heroNames[i]));
+    list.appendChild(entry);
+  }
+}
+function getHeroNames(){
+  var dummyData = ["Knight-Questor", "Tenebrael Shard",
+  "Fyreslayer Doomseeker", "Mistweaver Saih"];
+  return dummyData;
 }
