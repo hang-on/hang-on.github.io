fbe2e1412a41d3d3990f7de0839803dcf1400bf0
hang-on <hang-on@users.noreply.github.com>
2020-04-25 09:38:15 +0200

    sd

--- oracle/javascript.js
+++ oracle/javascript.js
@@ -10,38 +10,61 @@ heroPage = {
         Item: function(props){}
     }
 }
-
-function alertWindowDimension(){
-  var w = getWindowWidth();
-  var h = getWindowWHeight();
-  alert("Width: " + w + " Height: " + h);
+function isEven(value) {
+	if (value%2 == 0)
+		return true;
+	else
+		return false;
 }
-function getWindowWidth(){
-  var w = window.innerWidth
-  || document.documentElement.clientWidth
-  || document.body.clientWidth;
-  return w;
-}
-function getWindowWHeight(){
-  var h = window.innerHeight
-  || document.documentElement.clientHeight
-  || document.body.clientHeight;
-  return h;
-}
-function initializeHeroPage(){
-  // Populate the Hero Menu
-  var heroNames = [];
-  heroNames = getHeroNames();
-  list = document.getElementById("heroMenu");
-  var i;
-  for (i = 0; i < heroNames.length; i++){
-    var entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(heroNames[i]));
-    list.appendChild(entry);
+function getAnswer(modifier, chaosFactor){
+  var answer;
+  var oracleDice = Math.floor(Math.random() * 12) + 1;
+  var oracleScore = oracleDice + modifier;
+  var x = Math.floor(Math.random() * 100) + 1
+  if (x <= chaosFactor){
+    if (isEven(x)){
+      oracleScore --;
+    } else {
+      oracleScore ++;
+    }
   }
+  //
+  if (oracleScore < 1){
+    oracleScore = 1;
+  } else if (oracleScore > 12) {
+    oracleScore = 12;
+  }
+  switch (oracleScore){
+    case 1:
+      answer = "No, and...";
+    break;
+    case 2:
+    case 3:
+    case 4:
+      answer = "No";
+    break;
+    case 5:
+    case 6:
+      answer = "No, but...";
+    break;
+    case 7:
+    case 8:
+      answer = "Yes, but...";
+    break;
+    case 9:
+    case 10:
+    case 11:
+      answer = "Yes";
+    break;
+    case 12:
+      answer = "Yes, and...";
+    break;
+    default:
+      answer = "Oracle Error!";
+  }
+  document.getElementById("answer").innerHTML = "The Oracle seems to be thinking... ";
+  setTimeout(function(){ displayAnswer(answer); }, 1500);
 }
-function getHeroNames(){
-  var dummyData = ["Knight-Questor", "Tenebrael Shard",
-  "Fyreslayer Doomseeker", "Mistweaver Saih"];
-  return dummyData;
+function displayAnswer(answer){
+    document.getElementById("answer").innerHTML = "The answer is: " + answer;
 }
