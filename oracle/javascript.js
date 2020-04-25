3ae3db2ef4f741c0e0f26c6f01bd6fd70c3d954d
hang-on <hang-on@users.noreply.github.com>
2020-04-25 07:37:46 +0200

    sd

---
+++ oracle/javascript.js
@@ -0,0 +1,47 @@
+heroPage = {
+    config:{
+        prop:1
+    },
+    settings:{
+        currentHero:0
+    },
+    widgets:{
+        List: function(props){},
+        Item: function(props){}
+    }
+}
+
+function alertWindowDimension(){
+  var w = getWindowWidth();
+  var h = getWindowWHeight();
+  alert("Width: " + w + " Height: " + h);
+}
+function getWindowWidth(){
+  var w = window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+  return w;
+}
+function getWindowWHeight(){
+  var h = window.innerHeight
+  || document.documentElement.clientHeight
+  || document.body.clientHeight;
+  return h;
+}
+function initializeHeroPage(){
+  // Populate the Hero Menu
+  var heroNames = [];
+  heroNames = getHeroNames();
+  list = document.getElementById("heroMenu");
+  var i;
+  for (i = 0; i < heroNames.length; i++){
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(heroNames[i]));
+    list.appendChild(entry);
+  }
+}
+function getHeroNames(){
+  var dummyData = ["Knight-Questor", "Tenebrael Shard",
+  "Fyreslayer Doomseeker", "Mistweaver Saih"];
+  return dummyData;
+}
